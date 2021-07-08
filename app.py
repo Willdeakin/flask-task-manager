3d0cd9a9a4296b2d8bfdc2a78cda6f7f2a1b6e8f
Willdeakin <willdeakin@outlook.com>
2021-07-08 14:41:43 +0000

    add categories dropdown

--- app.py
+++ app.py
@@ -5,19 +5,17 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
-
 if os.path.exists("env.py"):
     import env
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
-
 
 
 @app.route("/")
@@ -27,7 +25,7 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 
-@app.route("/register", methods = ["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         # check if username already exists in db
@@ -48,6 +46,7 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
         return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -80,22 +79,21 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/profile/<username>", methods = ["GET","POST"])
+@app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-    #grab the session user's username from the database
+    # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
+
     if session["user"]:
         return render_template("profile.html", username=username)
+
     return redirect(url_for("login"))
-
-
-    return render_template("login.html")
 
 
 @app.route("/logout")
 def logout():
-    #remove user from session cookies
+    # remove user from session cookie
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
@@ -107,4 +105,6 @@ def add_task():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug = True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
